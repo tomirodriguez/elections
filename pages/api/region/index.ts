4e31faa301b64184966f94a8a9aaab8b9c183af8
Tomas Rodriguez <tomirodriguez.89@gmail.com>
2021-08-29 20:48:50 -0300

    reguis

--- pages/api/region/index.ts
+++ pages/api/region/index.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import fs from "fs";
 import type { NextApiRequest, NextApiResponse } from "next";
+import path from "path";
 import ApiResponse from "../../../model/ApiResponse";
+
+const jsonFiles = path.join(process.cwd(), "data");
+
+console.log(path);
+
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<ApiResponse>
 ) {
-  const fileData = fs.readFileSync("data/regions.json", "utf-8");
+  const fileData = fs.readFileSync(jsonFiles + "/regions.json", "utf-8");
 
   const regions = JSON.parse(fileData);
   res.status(200).json({
