7ad63666f5005a7a554f36d90ecc8945338a8bce
Tomi Rodriguez <tomirodriguez.89@gmail.com>
2021-09-03 18:28:04 -0300

    total data

--- pages/api/state/[categoryId]/index.js
+++ pages/api/state/[categoryId]/index.js
@@ -32,15 +32,83 @@ export default async function handler(req, res) {
       })
     );
 
+    let total = {
+      estadoRecuento: {
+        mesasEsperadas: 0,
+        mesasTotalizadas: 0,
+        cantidadElectores: 0,
+        cantidadVotantes: 0,
+      },
+      valoresTotalizadosOtros: {
+        votosNulos: 0,
+        votosEnBlanco: 0,
+        votosRecurridosComandoImpugnados: 0,
+        votosEnBlancoPorcentaje: 0,
+        votosRecurridosComandoImpugnadosPorcentaje: 0,
+        votosNulosPorcentaje: 0,
+      },
+      valoresTotalizadosPositivos: []
+    }
+
+    let totalVotes = 0
+
+    resultsByRegion.forEach(region => {
+      const regionData = region.state
+      Object.getOwnPropertyNames(total.estadoRecuento).forEach(prop => {
+        total.estadoRecuento[prop] += regionData.estadoRecuento[prop]
+      })
+      Object.getOwnPropertyNames(total.valoresTotalizadosOtros).forEach(prop => {
+        total.valoresTotalizadosOtros[prop] += regionData.valoresTotalizadosOtros[prop]
+      })
+
+      regionData.valoresTotalizadosPositivos.forEach(agrupation => {
+        const agrupationFound = total.valoresTotalizadosPositivos.find(agrupationInArray => agrupationInArray.idAgrupacion === agrupation.idAgrupacion)
+        if (!agrupationFound) {
+          total.valoresTotalizadosPositivos.push(agrupation)
+        } else {
+          agrupationFound.votos += agrupation.votos;
+          agrupation.listas.forEach(list => {
+            const stored = agrupationFound.listas.find(listStored => listStored.idLista === list.idLista);
+            if (!stored) {
+              agrupation.listas.push(list)
+            } else {
+              stored.votos += list.votos
+            }
+          })
+        }
+        totalVotes += agrupation.votos
+      })
+
+      total.valoresTotalizadosPositivos.forEach(agrupation => {
+        agrupation.votosPorcentaje = agrupation.votos * 100 / totalVotes;
+        agrupation.listas.forEach(list => {
+          list.votosPorcentaje = list.votos * 100 / agrupation.votos
+        })
+      })
+
+    });
+
+    total.estadoRecuento.mesasTotalizadasPorcentaje = total.estadoRecuento.mesasTotalizadas * 100 / total.estadoRecuento.mesasEsperadas;
+    total.estadoRecuento.participacionPorcentaje = total.estadoRecuento.cantidadVotantes * 100 / total.estadoRecuento.cantidadElectores;
+
+    total.valoresTotalizadosOtros.votosEnBlancoPorcentaje = total.valoresTotalizadosOtros.votosEnBlanco * 100 / total.estadoRecuento.cantidadVotantes;
+    total.valoresTotalizadosOtros.votosRecurridosComandoImpugnadosPorcentaje = total.valoresTotalizadosOtros.votosRecurridosComandoImpugnados * 100 / total.estadoRecuento.cantidadVotantes;
+    total.valoresTotalizadosOtros.votosNulosPorcentaje = total.valoresTotalizadosOtros.votosNulos * 100 / total.estadoRecuento.cantidadVotantes;
+
+
+    total.estadoRecuento.votosTotales = totalVotes;
+
+
+
     res.status(200).json({
       error: false,
       message: "Data successfully fetched!",
-      response: resultsByRegion,
+      response: { total, regions: resultsByRegion },
     });
   } catch (error) {
     res.status(404).json({
       error: true,
-      message: "Category not found!",
+      message: error.message,
       response: null,
     });
   }
