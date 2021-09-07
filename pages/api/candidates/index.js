f16130cec630ccf9789e789dcd7b040ba23c7a41
Tomi Rodriguez <tomirodriguez.89@gmail.com>
2021-09-07 12:10:14 -0300

    to js

--- pages/api/candidates/index.js
+++ pages/api/candidates/index.js
@@ -1,25 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
-import ApiResponse from "../../../model/ApiResponse";
-const regions = require("../../../api-elecciones/utils/regionsParser");
+const candidates = require("../../../api-elecciones/utils/candidatesParser");
 const token = require("../../../api-elecciones/utils/getToken");
 const ApiFetcher = require("../../../api-elecciones/utils/axios").ApiFetcher;
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<ApiResponse>
+  req,
+  res
 ) {
   // Chequear si esta en base de datos. Si no esta, pedirlo
   const tokenResponse = await token.getToken();
   const fetcher = new ApiFetcher(tokenResponse);
-  const regionsByCategory = await regions.getRegionsByCategeory(fetcher);
+  const candidatesByCategory = await candidates.getCandidatesByCategeory(
+    fetcher
+  );
   // Guardar en base de datos
 
   res.status(200).json({
     error: false,
     message: "Data successfully fetched!",
     response: {
-      categories: regionsByCategory,
+      candidates: candidatesByCategory,
     },
   });
 }
--- pages/api/regions-no-hierarchy/[categoryId]/index.js
+++ pages/api/regions-no-hierarchy/[categoryId]/index.js
@@ -2,28 +2,8 @@
 const regions = require("../../../../api-elecciones/utils/regionsParser");
 const token = require("../../../../api-elecciones/utils/getToken");
 const ApiFetcher = require("../../../../api-elecciones/utils/axios").ApiFetcher;
-import Cors from 'cors';
 
-// Initializing the cors middleware
-const cors = Cors({
-  methods: ['GET', 'HEAD'],
-})
-
-// Helper method to wait for a middleware to execute before continuing
-// And to throw an error when an error happens in a middleware
-function runMiddleware(req, res, fn) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result) => {
-      if (result instanceof Error) {
-        return reject(result)
-      }
-
-      return resolve(result)
-    })
-  })
-}
-
-export default async function handler(req, res) {
+export default function handler(req, res) {
 
   await runMiddleware(req, res, cors)
 
